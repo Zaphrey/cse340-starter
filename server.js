37256daf4ed9110ffe07b255dffcf1f1ba0f1ade
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-11-27 19:41:33 -0500

    completed w04 learning activities

--- server.js
+++ server.js
@@ -5,15 +5,43 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const bodyParser = require("body-parser")
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const errorRoute = require("./routes/errorTestRoute")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
+const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View engine and templates
@@ -31,6 +59,7 @@ app.set("layout", "./layouts/layout"); // Not at views root
 // })
 app.use(static)
 app.use("/inv", inventoryRoute)
+app.use("/account", accountRoute)
 app.use("/", errorRoute)
 
 // index route
