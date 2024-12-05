e27276421b3c4a1d650e630c70d75fe9efa104b1
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-12-05 11:50:10 -0500

    completed team activity

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -43,10 +43,13 @@ invCont.buildDetailByInvId = async (req, res, next) => {
 invCont.buildManagementView = async (req, res, next) => {
   let nav = await utilities.getNav()
 
+  const classificationSelect = await utilities.buildClassificationList()
+
   res.render("./inventory/management", {
     title: "Management",
     nav,
     errors: null,
+    classificationSelect
   })
 }
 
@@ -124,4 +127,155 @@ invCont.addInventory = async (req, res) => {
   res.redirect("/inv/add-inventory")
 }
 
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
+
+/* ***************************
+ *  Edit inventory route
+ * ************************** */
+invCont.editInventory = async (req, res, next) => {
+  const inventory_id = parseInt(req.params.inventory_id)
+  const nav = await utilities.getNav()
+  const itemData = await invModel.getDetailsByInventoryId(inventory_id)
+  const classificationList = await utilities.buildClassificationList(itemData.classification_id)
+  const name = `${ itemData.inv_make } ${ itemData.inv_model }`
+
+  res.render("./inventory/edit-inventory", {
+    title: `Editing ${ name }`,
+    nav,
+    errors: null,
+    classificationList: classificationList,
+    inv_id: itemData.inv_id,
+    inv_make: itemData.inv_make,
+    inv_model: itemData.inv_model,
+    inv_year: itemData.inv_year,
+    inv_description: itemData.inv_description,
+    inv_image: itemData.inv_image,
+    inv_thumbnail: itemData.inv_thumbnail,
+    inv_price: itemData.inv_price,
+    inv_miles: itemData.inv_miles,
+    inv_color: itemData.inv_color,
+    classification_id: itemData.classification_id
+  })
+}
+
+/* ***************************
+ *  Update Inventory Data
+ * ************************** */
+invCont.updateInventory = async function (req, res, next) {
+  try {
+    let nav = await utilities.getNav()
+    const {
+      inv_id,
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+      classification_id,
+    } = req.body
+    const updateResult = await invModel.updateInventory(
+      inv_id,  
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+      classification_id
+    )
+
+    if (updateResult) {
+      const itemName = updateResult.inv_make + " " + updateResult.inv_model
+      req.flash("notice", `The ${itemName} was successfully updated.`)
+      res.redirect("/inv/")
+    } else {
+      const classificationSelect = await utilities.buildClassificationList(classification_id)
+      const itemName = `${inv_make} ${inv_model}`
+      req.flash("notice", "Sorry, the insert failed.")
+      res.status(501).render("inventory/edit-inventory", {
+      title: "Editing " + itemName,
+      nav,
+      classificationSelect: classificationSelect,
+      errors: null,
+      inv_id,
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color,
+      classification_id
+      })
+    }
+  } catch (error) {
+    console.error("updateInventory " + error)
+  }
+}
+
+/* ***************************
+ *  Deliver inventory deletion view
+ * ************************** */
+invCont.buildDeleteView = async (req, res) => {
+  const inventory_id = parseInt(req.params.inventory_id)
+  const nav = await utilities.getNav()
+  const { inv_make, inv_model, inv_year, inv_price } = await invModel.getDetailsByInventoryId(inventory_id)
+  
+  res.render("./inventory/delete-confirm", {
+    title: "Delete Confirmation - " +`${ inv_year } ${ inv_make } ${ inv_model }`,
+    nav,
+    errors: null,
+    inv_id: inventory_id,
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_price
+  })
+}
+
+/* ***************************
+ *  Route for deleting items from the inventory
+ * ************************** */
+invCont.deleteInventoryFromId = async function (req, res, next) {
+  try {
+    let nav = await utilities.getNav()
+    const inv_id = req.body.inv_id
+    console.log(req.body)
+    const result = await invModel.deleteInventory(inv_id)
+
+    if (result) {
+      req.flash("notice", `The item was successfully deleted.`)
+      res.redirect("/inv/")
+    } else {
+      req.flash("notice", "Sorry, the delete failed.")
+      res.status(501).render("inventory/delete-confirm", {
+      title: "Delete Confirmation",
+      nav,
+      errors: null,
+      })
+    }
+  } catch (error) {
+    console.error("deleteInventory " + error)
+  }
+}
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,14 +1,42 @@
 const router = require("express").Router();
-const { buildByClassificationId, buildDetailByInvId, buildManagementView, buildNewClassificationView, addClassification, buildInventoryView, addInventory } = require("../controllers/inventoryController");
+const { buildByClassificationId, buildDetailByInvId, buildManagementView, buildNewClassificationView, addClassification, buildInventoryView, addInventory, getInventoryJSON, editInventory, updateInventory, buildDeleteView, deleteInventoryFromId } = require("../controllers/inventoryController");
 const { handleErrors } = require("../utilities");
-const { classifcationRules, checkClassificationData, inventoryRules, checkInventoryData } = require("../utilities/inventory-validation");
+const { classifcationRules, checkClassificationData, inventoryRules, checkInventoryData, checkUpdateData } = require("../utilities/inventory-validation");
 
+// Deliver a view that contains a list of vehicles dependant on the classification_id
 router.get("/type/:classificationId", handleErrors(buildByClassificationId))
+
+// Deliver vehicle detail page from inventory_id
 router.get("/detail/:inventoryId", handleErrors(buildDetailByInvId))
+
+// Deliver management view
 router.get("/", handleErrors(buildManagementView))
-router.get("/add-classification", handleErrors(buildNewClassificationView))
-router.post("/add-classification", classifcationRules(), checkClassificationData, handleErrors(addClassification))
-router.get("/add-inventory", handleErrors(buildInventoryView))
-router.post("/add-inventory", inventoryRules(), checkInventoryData, handleErrors(addInventory))
+
+// Deliver classification addition form
+router.get("/addClassification", handleErrors(buildNewClassificationView))
+
+// Add new classification to classifications table
+router.post("/addClassification", classifcationRules(), checkClassificationData, handleErrors(addClassification))
+
+// Deliver inventory addition form
+router.get("/addInventory", handleErrors(buildInventoryView))
+
+// Add new vehicle to inventory
+router.post("/addInventory", inventoryRules(), checkInventoryData, handleErrors(addInventory))
+
+// Get inventory by classification id
+router.get("/getInventory/:classification_id", handleErrors(getInventoryJSON))
+
+// Route for editing inventory items
+router.get("/edit/:inventory_id", handleErrors(editInventory))
+
+// Route for updaing inventory items
+router.post("/update", inventoryRules(), checkUpdateData, handleErrors(updateInventory))
+
+// Route for delivering inventory deletion view
+router.get("/delete/:inventory_id", handleErrors(buildDeleteView))
+
+// DELETE route for deleting items from the inventory.
+router.post("/delete/", handleErrors(deleteInventoryFromId))
 
 module.exports = router
